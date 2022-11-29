fe8b837e528a6b5636cdd785390465fbeb9e4d0d
Taylan <taylanalparslan@hotmail.com>
2022-11-29 19:09:22 +0300

    react-hook-form added.

--- src/pages/AddNote/AddNote.jsx
+++ src/pages/AddNote/AddNote.jsx
@@ -1,67 +1,138 @@
-import { Box, Button, TextField } from "@mui/material";
-import { Container } from "@mui/system";
 import React from "react";
+import { Controller, useForm } from "react-hook-form";
+import { Box, Button, Container, TextField } from "@mui/material";
 import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
 import { createNote } from "../../redux/features/note/noteSlice";
+import { useNavigate } from "react-router-dom";
 
 export default function AddNote() {
+  const { handleSubmit, reset, control } = useForm();
 
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const [values, setValues] = React.useState({
-   title:"",
-   description:""
-  });
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const handleChange = (prop) => (event) => {
-    setValues({ ...values, [prop]: event.target.value });
+  const onSubmit = (data) => {
+    dispatch(createNote(data)).then(() => navigate("/"));
   };
 
-  const handleSubmit = async () => {
-    dispatch(createNote(values))
-    navigate("/")
-  }
-
   return (
-    <div>
-      <Container maxWidth="xs" style={{ paddingTop: "2rem" }}>
-        <form>
-          <Box
-            component="form"
-            sx={{
-              "& > :not(style)": { m: 2, width: "50ch" },
-            }}
-            noValidate
-            autoComplete="off"
+    <Container maxWidth="xs" style={{ paddingTop: "2rem" }}>
+      <form>
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 2, width: "50ch" },
+          }}
+        >
+          <Controller
+            name={"title"}
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <TextField
+                id="outlined-basic"
+                label="Title"
+                value={value}
+                variant="outlined"
+                onChange={onChange}
+              />
+            )}
+          />
+          <Controller
+            name={"description"}
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <TextField
+                id="outlined-basic"
+                label="Description"
+                value={value}
+                variant="outlined"
+                onChange={onChange}
+              />
+            )}
+          />
+          <Button
+            onClick={handleSubmit(onSubmit)}
+            variant="contained"
+            color="success"
           >
-            <TextField 
-              id="outlined-basic" 
-              label="Title" 
-              value={values.title}
-              variant="outlined" 
-              onChange={handleChange('title')}
-
-            />
-            <TextField
-              id="outlined-basic"
-              label="Description"
-              variant="outlined"
-              value={values.description}
-              onChange={handleChange('description')}
-            />
-            <Button
-              variant="contained"
-              color="success"
-              // type="submit"
-              sx={{ maxWidth: "50%", alignItems: "center" }}
-              onClick = {() => handleSubmit()}
-            >
-              Ekle
-            </Button>
-          </Box>
-        </form>
-      </Container>
-    </div>
+            Ekle
+          </Button>
+          <Button onClick={() => reset()} variant="contained" color="primary">
+            Sıfırla
+          </Button>
+        </Box>
+      </form>
+    </Container>
   );
 }
+
+
+
+// import { Box, Button, TextField } from "@mui/material";
+// import { Container } from "@mui/system";
+// import React from "react";
+// import { useDispatch } from "react-redux";
+// import { useNavigate } from "react-router-dom";
+// import { createNote } from "../../redux/features/note/noteSlice";
+
+// export default function AddNote() {
+
+//   const dispatch = useDispatch()
+//   const navigate = useNavigate()
+//   const [values, setValues] = React.useState({
+//    title:"",
+//    description:""
+//   });
+
+//   const handleChange = (prop) => (event) => {
+//     setValues({ ...values, [prop]: event.target.value });
+//   };
+
+//   const handleSubmit = async () => {
+//     dispatch(createNote(values))
+//     navigate("/")
+//   }
+
+//   return (
+//     <div>
+//       <Container maxWidth="xs" style={{ paddingTop: "2rem" }}>
+//         <form>
+//           <Box
+//             component="form"
+//             sx={{
+//               "& > :not(style)": { m: 2, width: "50ch" },
+//             }}
+//             noValidate
+//             autoComplete="off"
+//           >
+//             <TextField
+//               id="outlined-basic"
+//               label="Title"
+//               value={values.title}
+//               variant="outlined"
+//               onChange={handleChange('title')}
+
+//             />
+//             <TextField
+//               id="outlined-basic"
+//               label="Description"
+//               variant="outlined"
+//               value={values.description}
+//               onChange={handleChange('description')}
+//             />
+//             <Button
+//               variant="contained"
+//               color="success"
+//               // type="submit"
+//               sx={{ maxWidth: "50%", alignItems: "center" }}
+//               onClick = {() => handleSubmit()}
+//             >
+//               Ekle
+//             </Button>
+//           </Box>
+//         </form>
+//       </Container>
+//     </div>
+//   );
+//}
+
--- src/pages/updateNote/UpdateNote.jsx
+++ src/pages/updateNote/UpdateNote.jsx
@@ -3,64 +3,138 @@ import { useSelector, useDispatch } from "react-redux";
 import { Container, Box, TextField, Button } from "@mui/material";
 import { updateNote } from "../../redux/features/note/noteSlice";
 import { useNavigate } from "react-router-dom";
+import { Controller, useForm } from "react-hook-form";
 
 export default function UpdateNote() {
   const { selectedNote } = useSelector((state) => state.note);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const { handleSubmit, reset, control } = useForm();
 
-  const [values, setValues] = React.useState({
-    id: selectedNote.id,
-    title: selectedNote.title,
-    description: selectedNote.description,
-  });
-
-  const handleChange = (prop) => (event) => {
-    setValues({ ...values, [prop]: event.target.value });
-  };
-
-  const handleSubmit = () => {
-    dispatch(updateNote(values));
-    navigate("/")
+  const onSubmit = (data) => {
+    dispatch(updateNote({ ...data, id: selectedNote.id })).then(() =>
+      navigate("/")
+    );
   };
 
   return (
-    <div>
-      <Container maxWidth="xs">
-        <form>
-          <Box
-            component="form"
-            sx={{
-              "& > :not(style)": { m: 2, width: "50ch" },
-            }}
-            noValidate
-            autoComplete="off"
+    <Container maxWidth="xs">
+      <form>
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 2, width: "50ch" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <Controller
+            name="title"
+            control={control}
+            render={({ field: { onChange, value = selectedNote.title } }) => (
+              <TextField
+                id="outlined-basic"
+                label="Title"
+                value={value}
+                variant="outlined"
+                onChange={onChange}
+              />
+            )}
+          />
+          <Controller
+            name="description"
+            control={control}
+            render={({ field: { onChange, value = selectedNote.title } }) => (
+              <TextField
+                id="outlined-basic"
+                label="description"
+                value={value}
+                variant="outlined"
+                onChange={onChange}
+              />
+            )}
+          />
+
+          <Button
+            onClick={handleSubmit(onSubmit)}
+            variant="contained"
+            color="success"
           >
-            <TextField
-              id="outlined-basic"
-              label="Title"
-              value={values.title}
-              variant="outlined"
-              onChange={handleChange("title")}
-            />
-            <TextField
-              id="outlined-basic"
-              label="Description"
-              variant="outlined"
-              value={values.description}
-              onChange={handleChange("description")}
-            />
-            <Button
-              variant="contained"
-              color="success"
-              sx={{ maxWidth: "50%", alignItems: "center" }}
-              onClick={() => handleSubmit()}
-            >
-              Güncelle
-            </Button>
-          </Box>
-        </form>
-      </Container>
-    </div>
+            Güncelle
+          </Button>
+          <Button onClick={() => reset()} variant="contained" color="primary">
+            Sıfırla
+          </Button>
+        </Box>
+      </form>
+    </Container>
   );
 }
+
+
+// import React from "react";
+// import { useSelector, useDispatch } from "react-redux";
+// import { Container, Box, TextField, Button } from "@mui/material";
+// import { updateNote } from "../../redux/features/note/noteSlice";
+// import { useNavigate } from "react-router-dom";
+
+// export default function UpdateNote() {
+//   const { selectedNote } = useSelector((state) => state.note);
+//   const dispatch = useDispatch();
+//   const navigate = useNavigate();
+
+//   const [values, setValues] = React.useState({
+//     id: selectedNote.id,
+//     title: selectedNote.title,
+//     description: selectedNote.description,
+//   });
+
+//   const handleChange = (prop) => (event) => {
+//     setValues({ ...values, [prop]: event.target.value });
+//   };
+
+//   const handleSubmit = () => {
+//     dispatch(updateNote(values));
+//     navigate("/")
+//   };
+
+//   return (
+//     <div>
+//       <Container maxWidth="xs">
+//         <form>
+//           <Box
+//             component="form"
+//             sx={{
+//               "& > :not(style)": { m: 2, width: "50ch" },
+//             }}
+//             noValidate
+//             autoComplete="off"
+//           >
+//             <TextField
+//               id="outlined-basic"
+//               label="Title"
+//               value={values.title}
+//               variant="outlined"
+//               onChange={handleChange("title")}
+//             />
+//             <TextField
+//               id="outlined-basic"
+//               label="Description"
+//               variant="outlined"
+//               value={values.description}
+//               onChange={handleChange("description")}
+//             />
+//             <Button
+//               variant="contained"
+//               color="success"
+//               sx={{ maxWidth: "50%", alignItems: "center" }}
+//               onClick={() => handleSubmit()}
+//             >
+//               Güncelle
+//             </Button>
+//           </Box>
+//         </form>
+//       </Container>
+//     </div>
+//   );
+// }
